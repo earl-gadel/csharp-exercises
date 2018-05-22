f9f459537e40c19475d935973113942223dc8aa4
Earl Gadel <earljg@gmail.com>
2018-05-22 17:49:32 -0500

    student class

--- School/Program.cs
+++ School/Program.cs
@@ -12,7 +12,24 @@ namespace School
 
     class Student
     {
+        private static int nextStudentId = 1;
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public int NumberOfCredits { get; set; }
+        public double Gpa { get; set; }
 
+        public Student(string name, int numberOfCredits,
+                        double gpa)
+        {
+            StudentId = nextStudentId++;
+            Name = name;
+            NumberOfCredits = numberOfCredits;
+            Gpa = gpa;
+        }
+
+        public Student(string name)
+            : this(name, 0, 0) { }
     }
+}
 
 }
